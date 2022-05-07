864e5937c247867497815b3bdfe34b827473c926
Kickofff <petr.trubetskoy@mail.ru>
2022-05-07 17:58:26 +0300

    bot

--- recipe_bot.py
+++ recipe_bot.py
@@ -3,7 +3,13 @@ from telebot import types
 import random
 token="5371708856:AAFjSjtyderlCBeg05W2K1FKXNAVLEQaDKg"
 bot=telebot.TeleBot(token)
-f = open('recipe.txt', 'r', encoding='UTF-8')
+f = open('Завтраки.json', 'r', encoding='UTF-8')
+recipe = f.read().split('\n')
+f.close()
+f = open('Обеды.json', 'r', encoding='UTF-8')
+recipe = f.read().split('\n')
+f.close()
+f = open('Ужины.json', 'r', encoding='UTF-8')
 recipe = f.read().split('\n')
 f.close()
 @bot.message_handler(commands=["start"])
@@ -15,6 +21,18 @@ def start(m, res=False):
 @bot.message_handler(content_types=["text"])
 def handle_text(message):
     if message.text.strip() == 'Рецепт' :
-        answer = random.choice(recipe)
+        item1=types.KeyboardButton("Завтраки")
+	item1=types.KeyboardButton("Обеды")
+	item1=types.KeyboardButton("Ужины")
+	bot.send_message(m.chat.id, 'Нажми: \nНа какой прием пищи вы хотите получить рецепт ',  reply_markup=markup)
     bot.send_message(message.chat.id, answer)
+@bot.message_handler(content_types=["text"])
+def handle_text(message):
+	if  message.text.strip() == 'Завтрак' :
+		 answer = random.choice(Завтраки)
+	elif message.text.strip() == 'Обеды' :
+		answer = random.choice(Обеды)
+	elif message.text.strip() == 'Ужины'
+		answer = random.choice(Ужины)
+	bot.send_message(message.chat.id, answer)
 bot.polling(none_stop=True, interval=0)
