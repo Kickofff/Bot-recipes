c5a2a8c6a82ffe0f345ae9484a3da24cdd042f87
Kickofff <petr.trubetskoy@mail.ru>
2022-06-30 19:03:32 +0300

    Bot

--- recipe_bot.py
+++ recipe_bot.py
@@ -7,17 +7,18 @@ bot=telebot.TeleBot(token)
 f = open('recipes.json', 'r', encoding='UTF-8')
 recipes = json.load(f)
 f.close()
-breakfast = []
-lunch = []
-dinner = []
-for i in range(len(recipes)):
+#breakfast = []
+#lunch = []
+#dinner = []
+eat = {}
+"""for i in range(len(recipes)):
     if "Завтраки" in recipes[i]["eating"]:
         breakfast.append(recipes[i])
     if "Обеды" in recipes[i]["eating"]:
         lunch.append(recipes[i])
     if "Ужины" in recipes[i]["eating"]:
         dinner.append(recipes[i])
-        
+ """       
                 
 def json_recipe_to_str(r):
     return f'*Блюдо:* {r["name"]}.\n*Состав:* {", ".join(r["ingredients"])}.\n*Алгоритм Приготовления:* {r["recipe"]}'.replace(".", "\.")
@@ -32,6 +33,8 @@ def start(m, res=False):
 
 @bot.message_handler(content_types=["text"])
 def handle_text(message):
+    global eat
+    print(eat)
     if message.text.strip() == 'Рецепт' :
         markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
         item2=types.KeyboardButton("Завтраки")
@@ -40,17 +43,29 @@ def handle_text(message):
         markup.add(item2)
         markup.add(item3)
         markup.add(item4)
-        bot.send_message(message.chat.id, 'Нажми: \nНа какой прием пищи вы хотите получить рецепт ',  reply_markup=markup)
- 
-    if  message.text.strip() == 'Завтраки' :
-        answer = json_recipe_to_str(random.choice(breakfast))
-        bot.send_message(message.chat.id, answer, parse_mode = "MarkdownV2")
-    elif message.text.strip() == 'Обеды' :
-        answer = json_recipe_to_str(random.choice(lunch))
-        bot.send_message(message.chat.id, answer, parse_mode = "MarkdownV2")
-    elif message.text.strip() == 'Ужины':
-        answer = json_recipe_to_str(random.choice(dinner))
-        bot.send_message(message.chat.id, answer, parse_mode = "MarkdownV2")
-
+        bot.send_message(message.chat.id, 'Нажми: \nНа какой прием пищи вы хотите получить рецепт? ',  reply_markup=markup)
+        
+        
+    if message.text.strip() == 'Завтраки' or message.text.strip() == 'Обеды' or message.text.strip() == 'Ужины' :
+        markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
+        item5=types.KeyboardButton("cold")
+        item6=types.KeyboardButton("hot")
+        markup.add(item5)
+        markup.add(item6)
+        eat[message.chat.id] = message.text.strip()
+        bot.send_message(message.chat.id, 'Нажми: \nКакой температуры вы бы хотели блюдо? ',  reply_markup=markup)
+        
+    if  message.text.strip() == 'cold' or message.text.strip() == 'hot' :
+        markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
+        item1=types.KeyboardButton("Рецепт")	
+        markup.add(item1) 
+        #убрать копипаст
+        L = []
+        for i in range(len(recipes)):
+            if eat[message.chat.id] in recipes[i]["eating"] and  message.text.strip() == recipes[i]["temperature"]:
+                L.append(recipes[i])
+        answer = json_recipe_to_str(random.choice(L))
+        bot.send_message(message.chat.id, answer, parse_mode = "MarkdownV2",  reply_markup=markup)
+        
 
 bot.polling(none_stop=True, interval=0)
